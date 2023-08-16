a4d704500c676ef205e5f5bf47b6632ccb041c8e
Misha Khvatov <crisscrossapplesauce14@gmail.com>
2023-08-15 20:04:57 -0400

    Re-structured the code so it's more readable.

--- neural_net.py
+++ neural_net.py
@@ -1,132 +1,80 @@
 import numpy as np
 import scipy.io as io
 
-def relu(layer):
-    return np.maximum(0, layer)
 
-def relu_derivative(x):
-    return np.where(x > 0, 1, 0)
+import numpy as np
 
-def softmax(layer):
-    exp_layer = np.exp(layer - np.max(layer))  # for numerical stability
-    return exp_layer / exp_layer.sum(axis=0, keepdims=True)
+class NeuralNetwork:
 
-def cross_entropy(y_true, y_pred):
-    y_pred = np.clip(y_pred, 1e-10, 1. - 1e-10)  # Ensure numerical stability
-    return -np.sum(y_true * np.log(y_pred))
+    def __init__(self, input_size, hidden_size_1, hidden_size_2, output_size):
+        sizes = [input_size, hidden_size_1, hidden_size_2, output_size]
 
-def forward_propagation(input_layer, weights_1, bias_1, weights_2, bias_2, weights_3, bias_3):
-    z_layer1 = np.dot(weights_1, input_layer) + bias_1
-    layer_1 = relu(z_layer1)
+        # Using the notation w_ij: weight from neuron i in layer L to neuron j in L+1
+        self.weights = [np.random.randn(sizes[i], sizes[i+1]) for i in range(len(sizes)-1)]
+        self.biases = [np.random.randn(size, 1) for size in sizes[1:]]
+        self.layers = [np.zeros((size, 1)) for size in sizes]
+
+    def relu(self, layer_vector):
+        return np.maximum(0, layer_vector)
+
+    def relu_derivative(self, layer_vector):
+        return np.where(layer_vector > 0, 1, 0)
+
+    def softmax(self, layer):
+        exp_layer = np.exp(layer - np.max(layer))  # for numerical stability
+        return exp_layer / exp_layer.sum(axis=0, keepdims=True)
+
+    def cross_entropy_loss(self, y_true, y_pred):
+        y_pred = np.clip(y_pred, 1e-10, 1. - 1e-10)  # Ensure numerical stability
+        return -np.sum(y_true * np.log(y_pred))
+
+    def forward_propagation(self, input_data):
+        self.layers[0] = input_data
+        for i in range(1, len(self.layers)):
+            z = np.dot(self.layers[i-1].T, self.weights[i-1]) + self.biases[i-1]
+            if i != len(self.layers) - 1:
+                self.layers[i] = self.relu(z)
+            else:
+                self.layers[i] = self.softmax(z)
+
+    def backward_propagation(self, true_vector):
+        
+        #Initilize adjustment arrays
+        
+        d_layers = [np.zeros(layer.shape) for layer in self.layers]
+        d_weights = [np.zeros(w.shape) for w in self.weights]
+        d_biases = [np.zeros(b.shape) for b in self.biases]
+
+        d_layers[-1] = self.layers[-1] - true_vector
+        for l in range(len(self.layers)-1, 0, -1):
+            # Compute gradients for weights and biases
+            d_weights[l-1] = np.dot(self.layers[l-1], d_layers[l].T)
+            d_biases[l-1] = np.sum(d_layers[l], axis=1, keepdims=True)
+            
+            if l != 1:  # No need to compute d_layers[0] as it's the input layer
+                d_layers[l-1] = np.dot(self.weights[l-1], d_layers[l]) * self.relu_derivative(self.layers[l-1])
+
+        return d_weights, d_biases
+
     
-    z_layer2 = np.dot(weights_2, layer_1) + bias_2
-    layer_2 = relu(z_layer2)
-    
-    z_output = np.dot(weights_3, layer_2) + bias_3
-    output_probabilities = softmax(z_output)
-    
-    return layer_1, layer_2, output_probabilities
-
-def backward_propagation(input_layer, true_labels, output_probabilities, layer_1, layer_2, weights_2, weights_3):
-    delta_output = output_probabilities - true_labels
-
-    dweights_3 = np.dot(delta_output, layer_2.T)
-    dbias_3 = np.sum(delta_output, axis=1, keepdims=True)
-
-    delta_layer2 = np.dot(weights_3.T, delta_output) * relu_derivative(layer_2)
-    dweights_2 = np.dot(delta_layer2, layer_1.T)
-    dbias_2 = np.sum(delta_layer2, axis=1, keepdims=True)
-
-    delta_layer1 = np.dot(weights_2.T, delta_layer2) * relu_derivative(layer_1)
-    dweights_1 = np.dot(delta_layer1, input_layer.T)
-    dbias_1 = np.sum(delta_layer1, axis=1, keepdims=True)
-
-    return dweights_1, dbias_1, dweights_2, dbias_2, dweights_3, dbias_3
-
-def hot_encode(value):
-    out_vector = np.zeros((10,1))
-    out_vector[value, 1] =1;
-    return out_vector
-
-# Initialize layers and weights
-input_size = 784
-hidden_size_1 = 16
-hidden_size_2 = 16
-output_size = 10
-
-#Initilize Hyper Parameters
-BATCH_SIZE = 128
-
-
-weights_1 = np.random.randn(hidden_size_1, input_size)
-bias_1 = np.random.randn(hidden_size_1, 1)
-weights_2 = np.random.randn(hidden_size_2, hidden_size_1)
-bias_2 = np.random.randn(hidden_size_2, 1)
-weights_3 = np.random.randn(output_size, hidden_size_2)
-bias_3 = np.random.randn(output_size, 1)
-
-# 1. Load the data
-data = io.loadmat("emnist-mnist.mat")
-
-# 2. Extract training and test datasets
-train_data = data["dataset"][0][0][0][0][0][0]
-train_labels = data["dataset"][0][0][0][0][0][1]
-test_data = data["dataset"][0][0][1][0][0][0]
-test_labels = data["dataset"][0][0][1][0][0][1]
-
-# 3. Convert to numpy arrays
-train_data_np = np.array(train_data)
-train_labels_np = np.array(train_labels)
-test_data_np = np.array(test_data)
-test_labels_np = np.array(test_labels)
-
-
-LEARNING_RATE = 0.01  
-
-#Training Loop
-while(True):
-    random_indices = np.arange(train_data.shape[0])
-    np.random.shuffle(random_indices)
-    train_data = train_data[random_indices]
-    train_labels = train_labels[random_indices]
-    loss_sum = 0
-    
-    gradient_sum = (
-        np.zeros((hidden_size_1, input_size)), 
-        np.zeros((hidden_size_1, 1)), 
-        np.zeros((hidden_size_2, hidden_size_1)), 
-        np.zeros((hidden_size_2, 1)), 
-        np.zeros((output_size, hidden_size_2)), 
-        np.zeros((output_size, 1))
-    )
-    
-    # Mini Batch
-    for i in range(BATCH_SIZE):
-        input_vector = train_data[i]
-        layers = forward_propagation(input_vector, weights_1, bias_1, weights_2, bias_2, weights_3, bias_3)
-        gradient_element = backward_propagation(input_vector, hot_encode(train_labels[i]), layers[2], layers[0], layers[1], weights_2, weights_3)
-       
-        gradient_sum = tuple(a + b for a, b in zip(gradient_sum, gradient_element))
-        loss_sum += cross_entropy(hot_encode(train_labels[i]), layers[2])
-    
-    # Average the loss
-    loss = loss_sum / BATCH_SIZE
-    gradient = tuple(x / BATCH_SIZE for x in gradient_sum)
-    
-    # Update Network using learning rate
-    weights_1 -= LEARNING_RATE * gradient[0]
-    bias_1 -= LEARNING_RATE * gradient[1]
-    weights_2 -= LEARNING_RATE * gradient[2]
-    bias_2 -= LEARNING_RATE * gradient[3]
-    weights_3 -= LEARNING_RATE * gradient[4]
-    bias_3 -= LEARNING_RATE * gradient[5]
-
-    if(loss < 0.1): break
-    
-    
-    
-     
 
 
 
+# # 1. Load the data
+# data = io.loadmat("emnist-mnist.mat")
+
+# # 2. Extract training and test datasets
+# train_data = data["dataset"][0][0][0][0][0][0]
+# train_labels = data["dataset"][0][0][0][0][0][1]
+# test_data = data["dataset"][0][0][1][0][0][0]
+# test_labels = data["dataset"][0][0][1][0][0][1]
+
+# # 3. Convert to numpy arrays
+# train_data_np = np.array(train_data)
+# train_labels_np = np.array(train_labels)
+# test_data_np = np.array(test_data)
+# test_labels_np = np.array(test_labels)
+
+
+# LEARNING_RATE = 0.01  
 
